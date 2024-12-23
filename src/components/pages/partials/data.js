55ec36f878ba055cec1c013e01413a7b485b2a7c
govindlohar <govindlohar3210@gmail.com>
2024-12-23 17:42:09 +0530

    p

--- src/components/pages/partials/data.js
+++ src/components/pages/partials/data.js
@@ -1,4 +1,4 @@
-export let url = "https://e-learning-server-ey4m.onrender.com";
+export let url = "https://e-learning-server-4e6e.onrender.com";
 export let clientUrl = process.env.REACT_APP_CLIENT_URL || `/${window.location.hostname}`;
 
 
